61fbe44fd8a437bb88f4bedf7c9898a1596e26a4
Harsh Gupta <97147437+2003HARSH@users.noreply.github.com>
2024-04-11 12:56:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 import streamlit as st
 import pickle
 import gensim
+import base64
 from gensim.utils import simple_preprocess
 from utils import conv_lower,remove_special,remove_stopwords,join_back,document_vector
 
 st.set_page_config(
         page_title="Sentiment Analysis on movie reviews",
 )
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background("./bg_image.avif")
+
 
 clf = pickle.load(open('clf.pkl', 'rb'))
 model = gensim.models.Word2Vec.load("word2vec.model")
